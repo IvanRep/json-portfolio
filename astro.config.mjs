a3a2f3b631e3fb28c15eac5dac49372bc5412ded
IvanRep <ivanparra.develop@gmail.com>
2024-03-12 10:10:00 +0100

    build: Fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://IvanRep.github.io',
+  site: 'https://ivanrep.github.io',
   base: '/json-portfolio',
 });
